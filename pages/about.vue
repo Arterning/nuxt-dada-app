14ecf6f1f691cd8baba19f6f0d1067843622ff7d
ning <ning.huang.ridson@gmail.com>
2025-05-31 16:57:18 +0800

    init

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+      <p>此页面将在 /about 路由下显示。</p>
+    </section>
+  </template>
+  
---
+++ server/api/test.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    // ... Do whatever you want here
+
+    return {
+        code: 200,
+        message: 'Hello from the test API!'
+    }
+  })
+  
